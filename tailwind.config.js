5e7fa5216e17b8cc407b4e3bb13a366bee6f9a7c
iputuanggak <iputuanggakrisna@gmail.com>
2022-05-04 08:51:28 +0700

    hero-section

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["poppins"],
+      },
+      colors: {
+        "dark-putu": "#11151C",
+        "primary-color": "#3F37C9",
+        "secondary-color": "#B5179E",
+        "hover-color": "#E7E5FF",
+        "surface-color": "#212D40",
+      },
+    },
+  },
+  plugins: [],
+};
