6ede6b15730d8f298a58e0d03ad04dc6379740bf
iputuanggak <iputuanggakrisna@gmail.com>
2022-05-08 00:00:22 +0700

    update-navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector('#nav-menu');
+const navMenu = document.querySelector("#nav-menu");
+const navItem = document.querySelectorAll("#nav-menu a");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+navItem.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
+  });
 });
