a971c3ea6af25b625cf2fb7487c12f6fc2fae777
iputuanggak <iputuanggakrisna@gmail.com>
2022-05-08 13:16:28 +0700

    interactive-navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,41 @@
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
-const navItem = document.querySelectorAll("#nav-menu a");
+const navItemMenu = navMenu.querySelectorAll("a");
+const sections = document.querySelectorAll("section");
+const navItem = document.querySelectorAll("header a");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
 
-navItem.forEach(function (elem) {
+navItemMenu.forEach((elem) => {
   elem.addEventListener("click", function () {
     hamburger.classList.toggle("hamburger-active");
     navMenu.classList.toggle("hidden");
   });
 });
+
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 2) {
+      current = section.getAttribute("id");
+      console.log(current);
+    }
+  });
+
+  navItem.forEach((elem) => {
+    // console.log(elem);
+    elem.classList.remove("nav-link-active");
+    var hastag = elem.getAttribute("href");
+    var linkId = hastag.substring(1);
+    if (linkId == current) {
+      elem.classList.add("nav-link-active");
+    }
+  });
+};
